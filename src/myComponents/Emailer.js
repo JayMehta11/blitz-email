a64d89d8aade1c756307eab6d27a1a59032efa66
Jay <jay.m@ahduni.edu.in>
2023-02-14 17:24:08 +0530

    final edit

--- src/myComponents/Emailer.js
+++ src/myComponents/Emailer.js
@@ -16,6 +16,9 @@ function Emailer() {
 
   const [messageBCC, setMessageBCC] = useState('');
 
+  const [subject, setsubject] = useState('');
+
+  const [textBox, settextBox] = useState('');
 
 
 
@@ -29,7 +32,13 @@ function Emailer() {
     else if(fieldType === "BCC"){
       setMessageBCC(e.target.value);
     }
-    console.log('You clicked the Chip.');
+    else if(fieldType === "subject"){
+      setsubject(e.target.value);
+    }
+    else if(fieldType === "textBox"){
+      settextBox(e.target.value);
+    }
+    // console.log('You clicked the Chip.');
   };
 
   const handleDelete = (i,fieldType) => {
@@ -49,7 +58,7 @@ function Emailer() {
       temp.splice(i,1)
       setBCC(temp)
     }
-    console.info('You clicked the delete icon.');
+    // console.info('You clicked the delete icon.');
   };
 
   const handleKeyDown = (e,fieldType) => {
@@ -76,66 +85,96 @@ function Emailer() {
     }
   };
 
+  const handleSubmit = () => {
+    // let arr = {
+    //     'Email' : email,
+    //     'CC': CC,
+    //     'BCC': BCC,
+    //     'SUBJECT': subject,
+    //     'TextBox': textBox, 
+    //   };
+    // console.log(arr)
+    console.log('Email: ' + email);
+    console.log('CC: ' + CC);
+    console.log('BCC: ' + BCC);
+    console.log('SUBJECT: ' + subject);
+    console.log('TextBox: ' + textBox);
+
+    alert('Email has been sent succesfully.')
+  };
+
   return (
     <div className='container'>
       <div className='container emailer'>
-        <table>
-          <tr>
-            <div className='d-flex align-items-center pt-4'>
-              <td>Email:</td> 
-              <td><TextField style={{marginLeft:"0.5rem",width:'43rem',boxSizing:'inherit',}} value={message} onChange={(e) => handleClick(e, "email")} onDelete={(e) => handleDelete(e,"email")} onKeyDown={(e)=>handleKeyDown(e,"email")} InputProps={{
+        
+          
+      <div className='email-grid align-self-center'>
+              Email: 
+              <TextField style={{
+              // width:'42rem',
+              // boxSizing:'inherit',
+            }} value={message} onChange={(e) => handleClick(e, "email")} onDelete={(e) => handleDelete(e,"email")} onKeyDown={(e)=>handleKeyDown(e,"email")} InputProps={{
               startAdornment:(
                 <InputAdornment postion="start">
                   {email.map((e,i)=><Chip key={i} onDelete={(e) => handleDelete(e,"email")} label={e} style={{margin:'3px',}}></Chip>)}
                 </InputAdornment>
               )
               }} type="email" name="email" placeholder='Enter your Email'></TextField>
-              </td>
-            </div> 
-          </tr>
-          <tr>
-            <div className='d-flex align-items-center pt-4'>
-              <td>CC:</td> 
-              <td><TextField style={{marginLeft:"0.5rem",width:'44rem',boxSizing:'inherit',}} value={messageCC} onChange={(e) => handleClick(e, "CC")} onDelete={(e) => handleDelete(e,"CC")} onKeyDown={(e)=>handleKeyDown(e,"CC")} InputProps={{
+              
+            
+          
+          
+            
+              CC: 
+              <TextField style={{
+              // width:'44rem',
+              // boxSizing:'inherit',
+            }} value={messageCC} onChange={(e) => handleClick(e, "CC")} onDelete={(e) => handleDelete(e,"CC")} onKeyDown={(e)=>handleKeyDown(e,"CC")} InputProps={{
               startAdornment:(
                 <InputAdornment postion="start">
                   {CC.map((e,i)=><Chip key={i} onDelete={(e) => handleDelete(e,"CC")} label={e} style={{margin:'3px',}}></Chip>)}
                 </InputAdornment>
               )
               }} type="email" name="CC" placeholder='CC'></TextField>
-              </td></div> 
-          </tr>
-          <tr>
-            <div className='d-flex align-items-center pt-4'>
-              <td>BCC:</td> 
-              <td><TextField style={{marginLeft:"0.5rem",width:"43rem",boxSizing:'inherit',}} value={messageBCC} onChange={(e) => handleClick(e, "BCC")} onDelete={(e) => handleDelete(e,"BCC")} onKeyDown={(e)=>handleKeyDown(e,"BCC")} InputProps={{
+              
+          
+          
+            
+              BCC: 
+              <TextField style={{
+              // width:"43rem",
+              // boxSizing:'inherit',
+            }} value={messageBCC} onChange={(e) => handleClick(e, "BCC")} onDelete={(e) => handleDelete(e,"BCC")} onKeyDown={(e)=>handleKeyDown(e,"BCC")} InputProps={{
               startAdornment:(
                 <InputAdornment postion="start">
-                  {BCC.map((e,i)=><Chip key={i} onDelete={handleDelete(e,"BCC")} label={e} style={{margin:'3px',}} ></Chip>)}
+                  {BCC.map((e,i)=><Chip key={i} onDelete={(e)=>handleDelete(e,"BCC")} label={e} style={{margin:'3px',}} ></Chip>)}
                 </InputAdornment>
               )
               }} type="email" name="BCC" placeholder='BCC'></TextField>
-              </td>
-              </div> 
-          </tr>
-          <tr>
-            <div className='d-flex align-items-center pt-4'>Subject: <TextField style={{marginLeft:"0.5rem",width:"42rem",boxSizing:'inherit',}} type="text" name="Subject" placeholder='SUBJECT'></TextField></div>
-          </tr>
-          <tr>  
-            <div className='textBox pt-4'><textarea style={{marginLeft:"0.5rem",width:"46rem",fontSize:"16px",borderColor:'lightgray'}} name="textBox" rows="4" cols="40"></textarea></div>        
-          </tr>
-        </table>
-        <div className='d-flex justify-content-between' style={{width:"40rem",}}>
-          <div className='buttonR btn btn-primary m-3 mx-3' cursor="pointer">Generate token</div>
-          <div className='buttonR btn btn-success m-3' cursor="pointer">Send Email(s)</div>
-        </div>
-        <div>
-          <textarea style={{width:"46rem",fontSize:"16px",}} name="textBoxToken" rows="4" cols="40" placeholder='Access token response:'></textarea>
-        </div>
-        <div>
-            <textarea style={{width:"46rem",fontSize:"16px",}} name="textBoxToken" rows="8" cols="40" placeholder='Send email(s) server response:'></textarea>
-        </div>
-        <div className='buttonR btn btn-danger m-3' cursor="pointer">Clear all data</div>
+              
+              
+          
+          
+            Subject: <TextField style={{
+            // width:"42rem",
+            // boxSizing:'inherit',
+            }} onChange={(e) => handleClick(e, "subject")} type="text" name="Subject" placeholder='SUBJECT'></TextField>
+          
+            </div>
+            <textarea className='mt-3' style={{ alignSelf: "center", width: "80%",fontSize:"16px",borderColor:'lightgray'}} onChange={(e) => handleClick(e, "textBox")} name="textBox" rows="4" cols="40"></textarea>        
+          
+        
+        <div className='d-flex justify-content-around'>
+          <div className='buttonR btn btn-primary m-3 mx-3 align-self-center' cursor="pointer">Generate token</div>
+          <div className='buttonR btn btn-success m-3 align-self-center' onClick={handleSubmit} cursor="pointer">Send Email(s)</div>
+          </div>
+        
+          <textarea style={{width:"46rem",fontSize:"16px", alignSelf: "center"}} name="textBoxToken" rows="4" cols="40" placeholder='Access token response:'></textarea>
+        
+        
+          <textarea className='mt-3' style={{width:"46rem",fontSize:"16px", alignSelf: "center"}} name="textBoxToken" rows="8" cols="40" placeholder='Send email(s) server response:'></textarea>
+        
+        <div className='buttonR btn btn-danger m-3 align-self-center' cursor="pointer">Clear all data</div>
 
       </div>
         
